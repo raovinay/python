f1443dcb67b065537b539d40982464d5cc5be866
raovinay <raovinay.java@yahoo.in>
2016-10-08 22:13:47 +0530

    removing unwanted files

--- RandomGridCreator.py
+++
@@ -1,9 +0,0 @@
-import random
-
-csv_file = open(r'D:\Technical\Python\MaxContinuousCount\random.csv', 'w')
-for i in range(0, 20):
-    line = []
-    for j in range(0, 20):
-        line.append(random.randint(1, 600))
-    line = list(map(lambda x:str(x), line))
-    csv_file.write(','.join(line)+'\n')
